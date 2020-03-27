b1ee56737bde951ccac2cc7ee8d562643e616e2f
Vinod Chauhan <vinodc45@gmail.com>
2020-03-27 18:25:37 +0530

    Updates

---
+++ precache-manifest.ef9746475554baf2ae0aa59a60cabe49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76ed2b86e34daf7afbe60fb682c10d5",
+    "url": "/COVID19/index.html"
+  },
+  {
+    "revision": "b462cd3279000f54a63b",
+    "url": "/COVID19/static/css/main.acb66040.chunk.css"
+  },
+  {
+    "revision": "bf00f18c118dd4277845",
+    "url": "/COVID19/static/js/2.e11aa750.chunk.js"
+  },
+  {
+    "revision": "b462cd3279000f54a63b",
+    "url": "/COVID19/static/js/main.3bed208a.chunk.js"
+  },
+  {
+    "revision": "9b4c3f2a868f828c77f3",
+    "url": "/COVID19/static/js/runtime-main.3a0692e2.js"
+  }
+]);

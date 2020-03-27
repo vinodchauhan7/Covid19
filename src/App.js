01177bb5ef6f8f17aa8a88fc09eadebbe0fbd0df
Vinod Chauhan <vinodc45@gmail.com>
2020-03-27 19:06:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -37,7 +37,8 @@ function App() {
 
   React.useEffect(() => {
     console.log("coutny");
-    axios.get("http://ip-api.com/json").then(response => {
+    axios.get("http://ip-api.com/json/?fields=country").then(response => {
+      console.log(response);
       setCurrentCountry(response.data.country);
     });
     if (data && currentCountry) {
